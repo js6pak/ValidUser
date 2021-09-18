117a9e3890498a1b92aa0d3bd813859dff0118bb
js6pak <kubastaron@hotmail.com>
2021-09-18 14:09:52 +0200

    Switch to the correct repo structure

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.aliucord.gradle")
+}
+
+version = "0.1.2"
+description = "Fixes an issue where mentions sometimes become invalid-user"
+
+aliucord {
+    changelog.set(file("CHANGELOG.md").readText())
+    author("6pak", 141580516380901376)
+    updateUrl.set("https://github.com/js6pak/ValidUser/releases/latest/download/updater.json")
+    buildUrl.set("https://github.com/js6pak/ValidUser/releases/latest/download/ValidUser.zip")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 30
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    discord("com.discord:discord:aliucord-SNAPSHOT")
+    api("com.github.Aliucord:Aliucord:main-SNAPSHOT")
+}
