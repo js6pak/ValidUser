c8745da332fa0bed2c417ccb10ce880e5a97d168
js6pak <kubastaron@hotmail.com>
2022-02-20 20:34:11 +0100

    Reformat

--- src/main/java/pl/js6pak/validuser/ValidUser.kt
+++ src/main/java/pl/js6pak/validuser/ValidUser.kt
@@ -55,189 +55,189 @@ class ValidUser : Plugin() {
         var chatList: WidgetChatList? = null
 
         patcher.patch(
-                WidgetChatList::class.java.getDeclaredConstructor(),
-                Hook { param: XC_MethodHook.MethodHookParam ->
-                    chatList = param.thisObject as WidgetChatList
-                })
+            WidgetChatList::class.java.getDeclaredConstructor(),
+            Hook { param: XC_MethodHook.MethodHookParam ->
+                chatList = param.thisObject as WidgetChatList
+            })
 
         patcher.patch(
-                WidgetChatListAdapterItemMessage::class.java.getDeclaredMethod(
-                        "getMessageRenderContext",
-                        Context::class.java,
-                        MessageEntry::class.java,
-                        Function1::class.java
-                ),
-                InsteadHook patch@{ param: XC_MethodHook.MethodHookParam ->
-                    val context = param.args[0] as Context
-                    val messageEntry = param.args[1] as MessageEntry
-                    val function1 = param.args[2]
+            WidgetChatListAdapterItemMessage::class.java.getDeclaredMethod(
+                "getMessageRenderContext",
+                Context::class.java,
+                MessageEntry::class.java,
+                Function1::class.java
+            ),
+            InsteadHook patch@{ param: XC_MethodHook.MethodHookParam ->
+                val context = param.args[0] as Context
+                val messageEntry = param.args[1] as MessageEntry
+                val function1 = param.args[2]
 
-                    (param.thisObject as WidgetChatListAdapterItemMessage).run {
-                        return@patch MessageRenderContext(
-                                context,
-                                messageEntry.message.id, // PATCH: set message as context id instead of author
-                                messageEntry.animateEmojis,
-                                messageEntry.nickOrUsernames,
-                                (this.adapter as WidgetChatListAdapter).data.channelNames,
-                                messageEntry.roles,
-                                R.b.colorTextLink,
-                                `WidgetChatListAdapterItemMessage$getMessageRenderContext$1`.INSTANCE,
-                                `WidgetChatListAdapterItemMessage$getMessageRenderContext$2`(this),
-                                ColorCompat.getThemedColor(context, R.b.theme_chat_spoiler_bg),
-                                ColorCompat.getThemedColor(context, R.b.theme_chat_spoiler_bg_visible),
-                                function1 as `WidgetChatListAdapterItemMessage$getSpoilerClickHandler$1`?,
-                                `WidgetChatListAdapterItemMessage$getMessageRenderContext$3`(this),
-                                `WidgetChatListAdapterItemMessage$getMessageRenderContext$4`(context)
-                        )
-                    }
+                (param.thisObject as WidgetChatListAdapterItemMessage).run {
+                    return@patch MessageRenderContext(
+                        context,
+                        messageEntry.message.id, // PATCH: set message as context id instead of author
+                        messageEntry.animateEmojis,
+                        messageEntry.nickOrUsernames,
+                        (this.adapter as WidgetChatListAdapter).data.channelNames,
+                        messageEntry.roles,
+                        R.b.colorTextLink,
+                        `WidgetChatListAdapterItemMessage$getMessageRenderContext$1`.INSTANCE,
+                        `WidgetChatListAdapterItemMessage$getMessageRenderContext$2`(this),
+                        ColorCompat.getThemedColor(context, R.b.theme_chat_spoiler_bg),
+                        ColorCompat.getThemedColor(context, R.b.theme_chat_spoiler_bg_visible),
+                        function1 as `WidgetChatListAdapterItemMessage$getSpoilerClickHandler$1`?,
+                        `WidgetChatListAdapterItemMessage$getMessageRenderContext$3`(this),
+                        `WidgetChatListAdapterItemMessage$getMessageRenderContext$4`(context)
+                    )
                 }
+            }
         )
 
         patcher.patch(
-                UserMentionNode::class.java.getDeclaredMethod(
-                        "renderUserMention",
-                        SpannableStringBuilder::class.java,
-                        UserMentionNode.RenderContext::class.java
-                ),
-                PreHook patch@{ param: XC_MethodHook.MethodHookParam ->
-                    @Suppress("UNCHECKED_CAST")
-                    val thisObject =
-                            param.thisObject as UserMentionNode<UserMentionNode.RenderContext>
-                    val spannableStringBuilder = param.args[0] as SpannableStringBuilder
-                    val renderContext = param.args[1] as UserMentionNode.RenderContext?
-                            ?: return@patch
+            UserMentionNode::class.java.getDeclaredMethod(
+                "renderUserMention",
+                SpannableStringBuilder::class.java,
+                UserMentionNode.RenderContext::class.java
+            ),
+            PreHook patch@{ param: XC_MethodHook.MethodHookParam ->
+                @Suppress("UNCHECKED_CAST")
+                val thisObject =
+                    param.thisObject as UserMentionNode<UserMentionNode.RenderContext>
+                val spannableStringBuilder = param.args[0] as SpannableStringBuilder
+                val renderContext = param.args[1] as UserMentionNode.RenderContext?
+                    ?: return@patch
 
-                    val userId = thisObject.userId
+                val userId = thisObject.userId
 
-                    if (invalidUsers.contains(userId)) {
-                        logger.verbose("Set [$userId] = <invalid>")
-                        setInvalidUser(renderContext.context, spannableStringBuilder, userId)
+                if (invalidUsers.contains(userId)) {
+                    logger.verbose("Set [$userId] = <invalid>")
+                    setInvalidUser(renderContext.context, spannableStringBuilder, userId)
+                    param.result = null
+                    return@patch
+                }
+
+                if (renderContext.userNames == null || !renderContext.userNames.containsKey(userId)) {
+                    val storeUser: StoreUser = StoreStream.getUsers()
+                    val users = storeUser.users
+
+                    if (!users.containsKey(userId)) {
                         param.result = null
-                        return@patch
-                    }
 
-                    if (renderContext.userNames == null || !renderContext.userNames.containsKey(userId)) {
-                        val storeUser: StoreUser = StoreStream.getUsers()
-                        val users = storeUser.users
-
-                        if (!users.containsKey(userId)) {
-                            param.result = null
-
-                            if (lockedUsers.contains(userId)) {
-                                logger.verbose("Waiting for $userId in ${renderContext.myId}")
-                                lockedUsers[userId]!!.add(renderContext.myId)
-                                setText(renderContext.context, spannableStringBuilder, "loading?")
-                                return@patch
-                            }
-
-                            logger.verbose("Fetching $userId")
-                            setText(renderContext.context, spannableStringBuilder, "loading")
-
-                            fun refreshMessages() {
-                                if (renderContext is MessageRenderContext && chatList != null) {
-                                    val adapter = WidgetChatList.`access$getAdapter$p`(chatList)
-                                    val data = adapter.internalData
-
-                                    for (messageId in lockedUsers[userId]!!) {
-                                        val index =
-                                                CollectionUtils.findIndex(data) { e: ChatListEntry? ->
-                                                    e is MessageEntry && e.message.id == messageId
-                                                }
-
-                                        if (index != -1) {
-                                            try {
-                                                adapter.notifyItemChanged(index)
-                                                logger.verbose("Refreshed message $messageId")
-                                            } catch (e: Throwable) {
-                                                logger.warn("Failed to refresh message $messageId: $e")
-                                            }
-                                        } else {
-                                            logger.warn("Failed to refresh message $messageId")
-                                        }
-                                    }
-                                }
-                            }
-
-                            lockedUsers[userId] = arrayListOf(renderContext.myId)
-                            storeUser.dispatcher.schedule {
-                                val (user, error) = RestAPI.getApi().userGet(userId).await()
-
-                                if (error != null) {
-                                    if (error is HttpException && error.a() == 404) {
-                                        invalidUsers.add(userId)
-                                        logger.verbose("Fetched [$userId] = <invalid>")
-
-                                        refreshMessages()
-                                    } else {
-                                        logger.error("Failed to fetch the user", error)
-                                    }
-
-                                    lockedUsers.remove(userId)
-                                    return@schedule
-                                }
-
-                                logger.verbose("Fetched [$userId] = ${if (user == null) "null" else CoreUser(user).username}")
-
-                                if (user != null) {
-                                    storeUser.notifyUserUpdated.invoke(user)
-                                    refreshMessages()
-                                }
-
-                                lockedUsers.remove(userId)
-                            }
-
+                        if (lockedUsers.contains(userId)) {
+                            logger.verbose("Waiting for $userId in ${renderContext.myId}")
+                            lockedUsers[userId]!!.add(renderContext.myId)
+                            setText(renderContext.context, spannableStringBuilder, "loading?")
                             return@patch
                         }
 
-                        logger.verbose("Set [$userId] = ${users[userId]!!.username}")
+                        logger.verbose("Fetching $userId")
+                        setText(renderContext.context, spannableStringBuilder, "loading")
 
-                        if (renderContext.userNames != null) {
-                            renderContext.userNames[userId] = users[userId]!!.username
+                        fun refreshMessages() {
+                            if (renderContext is MessageRenderContext && chatList != null) {
+                                val adapter = WidgetChatList.`access$getAdapter$p`(chatList)
+                                val data = adapter.internalData
+
+                                for (messageId in lockedUsers[userId]!!) {
+                                    val index =
+                                        CollectionUtils.findIndex(data) { e: ChatListEntry? ->
+                                            e is MessageEntry && e.message.id == messageId
+                                        }
+
+                                    if (index != -1) {
+                                        try {
+                                            adapter.notifyItemChanged(index)
+                                            logger.verbose("Refreshed message $messageId")
+                                        } catch (e: Throwable) {
+                                            logger.warn("Failed to refresh message $messageId: $e")
+                                        }
+                                    } else {
+                                        logger.warn("Failed to refresh message $messageId")
+                                    }
+                                }
+                            }
                         }
+
+                        lockedUsers[userId] = arrayListOf(renderContext.myId)
+                        storeUser.dispatcher.schedule {
+                            val (user, error) = RestAPI.getApi().userGet(userId).await()
+
+                            if (error != null) {
+                                if (error is HttpException && error.a() == 404) {
+                                    invalidUsers.add(userId)
+                                    logger.verbose("Fetched [$userId] = <invalid>")
+
+                                    refreshMessages()
+                                } else {
+                                    logger.error("Failed to fetch the user", error)
+                                }
+
+                                lockedUsers.remove(userId)
+                                return@schedule
+                            }
+
+                            logger.verbose("Fetched [$userId] = ${if (user == null) "null" else CoreUser(user).username}")
+
+                            if (user != null) {
+                                storeUser.notifyUserUpdated.invoke(user)
+                                refreshMessages()
+                            }
+
+                            lockedUsers.remove(userId)
+                        }
+
+                        return@patch
+                    }
+
+                    logger.verbose("Set [$userId] = ${users[userId]!!.username}")
+
+                    if (renderContext.userNames != null) {
+                        renderContext.userNames[userId] = users[userId]!!.username
                     }
                 }
+            }
         )
     }
 
     private fun setInvalidUser(
-            context: Context,
-            spannableStringBuilder: SpannableStringBuilder,
-            userId: Long
+        context: Context,
+        spannableStringBuilder: SpannableStringBuilder,
+        userId: Long
     ) {
         setText(context, spannableStringBuilder, "<@!$userId>")
     }
 
     private fun setText(
-            context: Context,
-            spannableStringBuilder: SpannableStringBuilder,
-            text: String
+        context: Context,
+        spannableStringBuilder: SpannableStringBuilder,
+        text: String
     ) {
         val arrayList = ArrayList<CharacterStyle>()
         val length = spannableStringBuilder.length
         arrayList.add(StyleSpan(1))
         arrayList.add(
-                BackgroundColorSpan(
-                        ColorCompat.getThemedColor(
-                                context,
-                                R.b.theme_chat_mention_background
-                        )
+            BackgroundColorSpan(
+                ColorCompat.getThemedColor(
+                    context,
+                    R.b.theme_chat_mention_background
                 )
+            )
         )
         arrayList.add(
-                ForegroundColorSpan(
-                        ColorCompat.getThemedColor(
-                                context,
-                                R.b.theme_chat_mention_foreground
-                        )
+            ForegroundColorSpan(
+                ColorCompat.getThemedColor(
+                    context,
+                    R.b.theme_chat_mention_foreground
                 )
+            )
         )
         spannableStringBuilder.append(text)
         for (characterStyle in arrayList) {
             spannableStringBuilder.setSpan(
-                    characterStyle,
-                    length,
-                    spannableStringBuilder.length,
-                    33
+                characterStyle,
+                length,
+                spannableStringBuilder.length,
+                33
             )
         }
     }
